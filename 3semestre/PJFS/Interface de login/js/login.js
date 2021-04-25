381b86054b177e6d0ee0688e974b574790b1f16c
igorhms <65043431+igorhms@users.noreply.github.com>
2021-04-25 16:03:42 -0300

    Update login.js

--- 3semestre/PJFS/Interface de login/js/login.js
+++ 3semestre/PJFS/Interface de login/js/login.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
         let $user = $("#user").val();
         let $pwd = $("#pwd").val();
         if ($user && $pwd){
-            $.getJSON("Interface%20de%20login/json/users.json", function($registros){
+            $.getJSON("https://igorhms.github.io/academicos//3semestre/PJFS/Interface%20de%20login/json/users.json", function($registros){
                 if($registros.usuarios.filter($usuario => $usuario.user == $user && $usuario.pwd == $pwd).length > 0)
                     alert("Usuario válido");
                 else alert ("Usuario inválido");
